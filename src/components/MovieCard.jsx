044b26b89632fccfaf004016ed0baf00998429c5
Michael <MDKuhlmann@zoho.com>
2025-01-16 11:28:19 -0600

    added skeleton loading state to MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ const MovieCard = ({ movie }) => {
 
   return (
     <Link to={`/${movie.imdbID}`} >
-      <div className="hover:card-hover-shadow transition-group group relative rounded-2xl hover:scale-105">
+      {/* <div className="hover:card-hover-shadow transition-group group relative rounded-2xl hover:scale-105">
         <div className="absolute inset-0 flex items-end rounded-2xl border border-[#b1b1b166] bg-gradient-to-t from-black/60 to-transparent">
           <div className="transition-secondary translate-y-8 p-4 text-white group-hover:translate-y-0">
             <h3 className="mb-2 text-xl font-bold">{movie.Title}</h3>
@@ -33,6 +33,27 @@ const MovieCard = ({ movie }) => {
           loading="lazy"
           alt="movie-poster"
           className="movie-card-img rounded-2xl object-cover"
+        />
+      </div> */}
+      <div className="hover:card-hover-shadow transition-group group relative rounded-2xl hover:scale-105">
+        <div className="absolute inset-0 flex items-end rounded-2xl border border-[#b1b1b166] bg-gradient-to-t from-black/60 to-transparent">
+          <div className="transition-secondary translate-y-8 p-4 text-white group-hover:translate-y-0">
+            <h3 className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2"></h3>
+            <div className="mt-4 space-x-4 opacity-0 transition-opacity delay-500 group-hover:opacity-100">
+              <span>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-24 mb-4">
+  
+                  </div> 
+              </span>
+              <span className="flex justify-start">
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-36  mr-2"></div>
+              <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-16 "></div>
+              </span>
+            </div>
+          </div>
+        </div>
+        <img
+          className="movie-card-img bg-gray-200 rounded-2xl object-cover"
         />
       </div>
     </Link>
