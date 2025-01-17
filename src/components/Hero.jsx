fe3e18d5275e21ded2ca18c6053961155fe7d67f
Michael <MDKuhlmann@zoho.com>
2025-01-16 21:15:23 -0600

    adjusted hero margins for mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@
         return (
           <>
             <div className="flex justify-center text-center text-white">
-              <div className="flex flex-col items-center mx-10 max-w-screen-lg">
+              <div className="flex flex-col items-center mx-6 max-w-screen-lg">
                 <h1>
                   Your site to find your next favorite show and we make it{" "}
                   <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-sky-400">
